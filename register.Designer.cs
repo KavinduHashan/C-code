fbb90c507431028acecff9457f15b25c7cad7ced
Kavindu Hashan <89184906+KavinduHashan@users.noreply.github.com>
2022-08-03 21:53:08 +0530

    Add files via upload

---
+++ register.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace ex1
+{
+    partial class register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(register));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.regusertxt = new System.Windows.Forms.TextBox();
+            this.regpasstxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(258, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Register Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(182, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter User Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(194, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Password";
+            // 
+            // regusertxt
+            // 
+            this.regusertxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regusertxt.Location = new System.Drawing.Point(361, 136);
+            this.regusertxt.Name = "regusertxt";
+            this.regusertxt.Size = new System.Drawing.Size(318, 30);
+            this.regusertxt.TabIndex = 3;
+            // 
+            // regpasstxt
+            // 
+            this.regpasstxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regpasstxt.Location = new System.Drawing.Point(361, 182);
+            this.regpasstxt.Name = "regpasstxt";
+            this.regpasstxt.Size = new System.Drawing.Size(318, 30);
+            this.regpasstxt.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(296, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 68);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "SUBMIT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(801, 501);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.regpasstxt);
+            this.Controls.Add(this.regusertxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "register";
+            this.Text = "register";
+            this.Load += new System.EventHandler(this.register_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox regusertxt;
+        private System.Windows.Forms.TextBox regpasstxt;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ register.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ex1
+{
+    public partial class register : Form
+    {
+        public register()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\code\C# (visual studio)\curd operations\ex1db.mdf;Integrated Security=True;Connect Timeout=30");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string n = regusertxt.Text;
+                string p = regpasstxt.Text;
+
+                String insertquery = "INSERT INTO log (userName, password) VALUES ('" +n + "','" + p + "')";
+                SqlCommand cmd = new SqlCommand(insertquery, conn);
+                
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Registration successfully..!");
+
+                login l = new login();
+                this.Hide();
+                l.Show();
+            }
+            catch (SqlException es)
+            {
+                MessageBox.Show("Error..!" + es);
+            }
+            finally
+            {
+                conn.Close();                  
+            }
+        }
+
+        private void register_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
